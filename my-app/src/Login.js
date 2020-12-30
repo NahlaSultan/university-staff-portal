5d2040bf7b4db2cf84ea97cdbafba8d011ce7a97
SaraAmgadSamir <saraamgadsamir@gmail.com>
2020-12-30 21:47:20 +0200

    Redirection

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -5,50 +5,79 @@ import { Link, Switch, Route, Redirect } from 'react-router-dom'
 import { render } from 'react-dom'
 
 export default function Login() {
-  let loggedIn = false
-  // const [logIn, setlogIn] = useState(0)
+  const [logIn, setlogIn] = useState("")
   const EmailRef = useRef()
   const PassRef = useRef()
-  function check(res) {
-    if (res.data == "reset your password") {
-      console.log("I entered")
-      loggedIn = true
-    }
-    else {
 
-    }
-
-  }
   function HandleEmail() {
     const body = { email: EmailRef.current.value, password: PassRef.current.value }
     //  console.log(body)
 
     axios
       .post('http://localhost:8000/login', body)
+      .then(res => {
+        setlogIn(res.data)
+        console.log(logIn)
+      });
 
-      .then(res => check(res));
 
 
     // callAPI()
   }
 
-  return (
-    <div>
-      Email:
-      <input ref={EmailRef} type="text" />
-      <br></br>
-    Password:
-      <input ref={PassRef} type="text" />
-      <br></br>
-      <Link to="/home"> <button onClick={HandleEmail}> login </button> </Link>
-      {/* <Route component={HRprofile} exact path="/home"/> */}
-      {/* <button onClick={HandleEmail}> login </button> */}
-      {/* <Route exact path="/">
-        {loggedIn? <Redirect to="/home" /> : <HRprofile />}
-      </Route> */}
+  if (logIn == "reset your password" || logIn == "Success") {
+    return (
+      <Redirect to="/home" />
+    )
+  }
+  else {
+    if (logIn == "Invalid password")
+      return (
+        <div>
+          <h2>Invalid password</h2>
+          Email:
+          <input ref={EmailRef} type="text" />
+          <br></br>
+          Password:
+          <input ref={PassRef} type="text" />
+          <br></br>
+          <button onClick={HandleEmail}> login </button>
 
-    </div>
-  )
+
+        </div>
+      )
+    else
+      if (logIn == "Invalid email") {
+        return (
+          <div>
+            <h2>Invalid email</h2>
+          Email:
+            <input ref={EmailRef} type="text" />
+            <br></br>
+          Password:
+            <input ref={PassRef} type="text" />
+            <br></br>
+            <button onClick={HandleEmail}> login </button>
+
+
+          </div>
+        )
+      }
+      else {
+        return (
+          <div>
+            Email:
+            <input ref={EmailRef} type="text" />
+            <br></br>
+            Password:
+            <input ref={PassRef} type="text" />
+            <br></br>
+            <button onClick={HandleEmail}> login </button>
+
+          </div>
+        )
+      }
+  }
 
 }
 
