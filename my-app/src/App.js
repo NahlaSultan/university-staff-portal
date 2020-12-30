5388ee615f9a8fcec9661089be10cbfba7c4f5f6
Nahla <nahlarsultan@gmail.com>
2020-12-30 21:55:37 +0200

    styling login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,29 @@
 
-import React,{useState,useRef} from 'react' 
-import './App.css';
-import Nav from './Nav'
-import Login from './Login'
-import AddStaff from './AddStaff'
-import {BrowserRouter as Router, Switch, Route}  from 'react-router-dom'
-import Home from './Home';
+// import React,{useState,useRef} from 'react' 
+// import './App.css';
+// import Nav from './Nav'
+// import Login from './Login'
+// import AddStaff from './AddStaff'
+// import {BrowserRouter as Router, Switch, Route}  from 'react-router-dom'
+// import Home from './Home';
 
-function App() {
+// function App() {
  
-  return (
-    <Router>  
-  <div className='App'> 
-   <h1> app page</h1>
-   <Nav/>
-   <Switch> 
-   <Route path='/' exact component={Home} />
+//   return (
+//     <Router>  
+//   <div className='App'> 
+//    <h1> app page</h1>
+//    <Nav/>
+//    <Switch> 
+//    <Route path='/' exact component={Home} />
 
-   <Route path='/hr/addStaff' component={AddStaff} />
-   <Route path='/login' component={Login} />
-   </Switch>
-    </div> 
-    </Router>   
-  )
+//    <Route path='/hr/addStaff' component={AddStaff} />
+//    <Route path='/login' component={Login} />
+//    </Switch>
+//     </div> 
+//     </Router>   
+//   )
 
-}
+// }
 
-export default App;
-
-    /*
-     <Nav/>
-      */
+// export default App;
