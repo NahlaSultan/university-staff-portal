90c7396e246b4a773c9c07c76965ec00eba882e1
SaraAmgadSamir <saraamgadsamir@gmail.com>
2020-12-30 19:16:51 +0200

    Redirecting trial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,18 @@
-import React,{useState,useRef} from 'react'
+import React, { useState, useRef } from 'react'
+import { Link, Switch, Route } from 'react-router-dom'
 import Login from './Login'
+import HRprofile from './HRprofile'
 function App() {
- 
+
   return (
     <>
-    <Login/>
-    
+      <Switch>
+        <Route component={Login} exact path="/" />
+        <Route component={HRprofile} exact path="/home"/>
+
+
+      </Switch>
+
     </>
   )
 
--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,29 +1,55 @@
-import React,{useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import axios from 'axios'
-export default function Login() {
-  const EmailRef=useRef()
-  const PassRef=useRef()
-  function HandleEmail(){
-    const body={email:EmailRef.current.value, password:PassRef.current.value  }
-  //  console.log(body)
+import HRprofile from './HRprofile'
+import { Link, Switch, Route, Redirect } from 'react-router-dom'
+import { render } from 'react-dom'
 
-   axios   
-   .post('http://localhost:8000/login', body)
-   
-   .then(res=>console.log(res.data));
-   // callAPI()
-}
+export default function Login() {
+  let loggedIn = false
+  // const [logIn, setlogIn] = useState(0)
+  const EmailRef = useRef()
+  const PassRef = useRef()
+  function check(res) {
+    if (res.data == "reset your password") {
+      console.log("I entered")
+      loggedIn = true
+    }
+    else {
+
+    }
+
+  }
+  function HandleEmail() {
+    const body = { email: EmailRef.current.value, password: PassRef.current.value }
+    //  console.log(body)
+
+    axios
+      .post('http://localhost:8000/login', body)
+
+      .then(res => check(res));
+
+
+    // callAPI()
+  }
 
   return (
     <div>
-     Email:
-    <input ref={EmailRef} type="text"/>
-    <br></br>
+      Email:
+      <input ref={EmailRef} type="text" />
+      <br></br>
     Password:
-    <input ref={PassRef} type="text"/>
-    <br></br>
-    <button onClick={HandleEmail}> login </button>
+      <input ref={PassRef} type="text" />
+      <br></br>
+      <Link to="/home"> <button onClick={HandleEmail}> login </button> </Link>
+      {/* <Route component={HRprofile} exact path="/home"/> */}
+      {/* <button onClick={HandleEmail}> login </button> */}
+      {/* <Route exact path="/">
+        {loggedIn? <Redirect to="/home" /> : <HRprofile />}
+      </Route> */}
+
     </div>
   )
+
 }
+
 
