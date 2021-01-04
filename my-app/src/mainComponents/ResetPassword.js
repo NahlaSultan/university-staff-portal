dc6b96c6f83033f9bbc089387f79e988721ba7af
SofiaNasser <you@email.com>
2021-01-04 15:52:37 +0200

    ..

--- my-app/src/mainComponents/ResetPassword.js
+++ my-app/src/mainComponents/ResetPassword.js
@@ -4,9 +4,14 @@ import '../styling/main.css';
 export default function ResetPassword() {
     const passRef=useRef()
     const conpassRef=useRef()
+    const [message, setMessage] = useState("")
+
   function HandleResetPassword(){
       console.log("heyy")
-      if(passRef==conpassRef){
+      if(passRef.current.value==conpassRef.current.value){
+        console.log("in if")
+        setMessage("")
+
     const body={password:passRef.current.value}
   //  console.log(body)
 
@@ -15,52 +20,10 @@ export default function ResetPassword() {
    
    .then(res=>console.log(res.data));}
    else{
-    console.log("heloo")
-    return (
-        <>
-        <div >
-            <div className="resetPassword">
-
-
-                <span className="login100-form-title">
-                    Reset Password
-      </span>
-                <div>
-                    <input required={true} ref={passRef} className="input100" name="password" placeholder="New Password" />
-                    <span className="focus-input100"></span>
-                    <span className="symbol-input100">
-                    </span>
-                    <br />
-                </div>
-                 <br></br>
-            <br></br>
-            <br></br>
-                <div>
-                    <input required={true} ref={conpassRef} className="input100" name="password" placeholder="confirm Password" />
-                    <span className="focus-input100"></span>
-                    <span className="symbol-input100">
-                    </span>
-                    <br />
-                </div>
-            </div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <h2>Not Matching!</h2>
-            <div className="buttons">
-                <button onClick={HandleResetPassword} className="buttons">
-                    Confirm
-        </button>
-            </div>
-
-        </div>
-
-    </>
-        
-    )
-   }
+    console.log("in else")
+    setMessage("passwords don't match!!")
    // callAPI()
+}
 }
     return (
         
@@ -91,6 +54,7 @@ export default function ResetPassword() {
                     </span>
                     <br />
                 </div>
+                <r1> {message}</r1>
             </div>
             <br></br>
             <br></br>
