e8542975b15014427b9c431bb63805fcfb636d58
SaraAmgadSamir <saraamgadsamir@gmail.com>
2021-01-15 11:34:23 +0200

    nav correct

--- my-app/src/academicMembersComponents/CancelLeaveRequest.js
+++ my-app/src/academicMembersComponents/CancelLeaveRequest.js
@@ -39,7 +39,7 @@ export default function CancelLeaveRequest() {
         setToggleReplacement(!toggleReplacement);
         //  console.log(toggle)
     }
-    useEffect(() => {
+      useEffect(() => {
         const checkToken = async () => {
             if (localStorage.getItem('token')) {
                 console.log("TOKENS")
@@ -59,7 +59,7 @@ export default function CancelLeaveRequest() {
 
         }
         checkToken()
-    });
+    }, []);
     useEffect(() => {
 
 
@@ -73,7 +73,7 @@ export default function CancelLeaveRequest() {
             });
     }, [])
 
-
+  
 
     function HandleCancel(e) {
         const body = { requestId: e.target.value }
